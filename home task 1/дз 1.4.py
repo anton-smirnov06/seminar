961c9505d393e8170f21f7e163e6d08d075a8b00
anton-smirnov06 <anton.sm-ov@yandex.ru>
2024-10-03 22:44:47 +0300

    Add files via upload

---
+++ home task 1/дз 1.4.py
@@ -0,0 +1,9 @@
+n=int(input())
+def triangle(h,depth=1, symbol='.'):
+    if h%2!=0 and depth == h//2 + 1:
+        print(symbol*depth)
+        return    
+    print(symbol*depth)
+    triangle(h,depth = depth+1)
+    print(symbol*depth)
+triangle(n)
